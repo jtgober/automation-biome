8d3e738b62d4a81b371512e8555e84a0ccaa76a4
jonathan gober <Gober.jonathan@gmail.com>
2022-06-28 21:47:28 -0400

    Added api call for data

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,42 @@
-<script>
+<script context="module">
+  // @ts-ignore
+  export async function load({ fetch }) {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const guides = await res.json();
 
+    if (res.ok) {
+      return {
+        props: {
+          guides,
+        },
+      };
+    }
+    return {
+      status: res.status,
+      error: new Error('Could not fetch')
+    };
+  }
 </script>
-<h1>This is the current landing page</h1>
+
+<script>
+  
+    export /**
+* @type {any}
+*/
+     let guides
+</script>
+
+<div class="guides">
+    <ul>
+        {#each guides as guide}
+        <li>
+            <a href="/">{guide.name}</a>
+        </li>
+
+        {/each}
+    </ul>
+</div>
 
 <style>
-h1{
-    text-align: center;
-}
+
 </style>
