fffc0a1ec0520d5f1b21ce890f47d92bc5228852
jonathan gober <Gober.jonathan@gmail.com>
2022-06-28 20:40:55 -0400

    playing with named layouts

---
+++ src/routes/guides/__layout-guides.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<main>
+  <slot />
+</main>
+
+<style>
+  main {
+    max-width: 980px;
+    margin: 20px auto;
+    font-size: xx-large;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,10 @@
 <script>
 
 </script>
-
+<h1>This is the current landing page</h1>
 
 <style>
-
+h1{
+    text-align: center;
+}
 </style>
