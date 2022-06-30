4304db8d724c59feb1ad9c56bd1dfbaf3591fbb5
jonathan gober <Gober.jonathan@gmail.com>
2022-06-30 12:53:48 -0400

    added tailwindcss

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,37 +1,8 @@
-<!-- <script>
-  import Title from "$lib/title.svelte";
-  import "../styles/global.css";
+<script>
+    import "../app.css"
 </script>
-
-<div>
-  <nav>
-    <a href="/">Home</a> |
-    <a href="/about">About</a> |
-    <a href="/guides">Guides</a> |
-  </nav>
-</div>
-<header>
-  <Title />
-</header> -->
 
 <main>
   <slot />
 </main>
-<!-- 
-<footer>
-  <p>This is a footer tag. Come add something in here</p>
-</footer>
 
-<style>
-  header {
-    display: flex;
-    justify-content: center;
-  }
-  main {
-    max-width: 980px;
-    margin: 20px auto;
-  }
-  footer {
-    text-align: center;
-  }
-</style> -->
