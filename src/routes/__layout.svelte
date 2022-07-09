084c8374dcf566a054f131220f04dc74d2da0f0b
jonathan gober <Gober.jonathan@gmail.com>
2022-07-09 13:52:55 -0400

    fixing nav

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,20 +11,3 @@
 </main>
 
 <Footer />
-
-<!-- <style>
-  :root {
-    --bg-color: #ffffff;
-    --text-color: #000000;
-  }
-
-  :global(body) {
-    background: var(--bg-color);
-    color: var(--text-color);
-  }
-
-  :global(body.dark) {
-    --bg-color: #000000;
-    --text-color: #ffffff;
-  }
-</style> -->
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,8 +1,10 @@
+
+
 <svelte:head>
   <title>Welcome</title>
 </svelte:head>
 
-<div
+<!-- <div
   class="flex items-center p-6 max-width-sm mx-auto bg-white rounded-xl shadow-lg space-x-4 mt-12"
 >
   <img class="w-10 h-10"src="./moon-outline.png" alt="" />
@@ -10,4 +12,5 @@
     <div class="text-xl font-medium text-black">Are you sure?</div>
     <p class="text-slate-500">You are about to delete a post</p>
   </div>
-</div>
+</div> -->
+
